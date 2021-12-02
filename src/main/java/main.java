1f83d60baf967c9bcef845ab2cfb67a81d63e010
ichibanmikan <ichibanmikan@hnu.edu.cn>
2021-12-02 19:53:46 +0800

    main调试部分

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+import util.DButil;
+
+import java.util.Date;
+
+public class main {
+    public static void main(String [] args){
+        System.out.println(new Date());
+        DButil dButil=new DButil();
+    }
+}
